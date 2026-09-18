535b016c9d753f7bd26e2232e2eb252973122ae0
Seu Nome <seu_email@example.com>
2026-09-17 23:56:31 -0300

    Adiciona projeto de fracoes

--- semana03/Fracoes/Program.cs
+++ semana03/Fracoes/Program.cs
@@ -1,9 +1,26 @@
-using System;
+Fracao fracao1 = new Fracao();
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Olá, Mundo! Este é o Projeto Fracoes.");
-    }
-}
+Console.WriteLine(fracao1.ObterFracaoEmTexto());
+Console.WriteLine(fracao1.GetNumerador());
+Console.WriteLine(fracao1.GetDenominador());
+
+Fracao fracao2 = new Fracao(5);
+
+Console.WriteLine(fracao2.ObterFracaoEmTexto());
+Console.WriteLine(fracao2.GetNumerador());
+
+Fracao fracao3 = new Fracao(3, 4);
+
+Console.WriteLine(fracao3.ObterFracaoEmTexto());
+Console.WriteLine(fracao3.ObterFracaoEmDecimal());
+
+Fracao fracao4 = new Fracao(1, 3);
+
+Console.WriteLine(fracao4.ObterFracaoEmTexto());
+Console.WriteLine(fracao4.ObterFracaoEmDecimal());
+
+fracao3.SetNumerador(1);
+fracao3.SetDenominador(2);
+
+Console.WriteLine(fracao3.ObterFracaoEmTexto());
+Console.WriteLine(fracao3.ObterFracaoEmDecimal());
